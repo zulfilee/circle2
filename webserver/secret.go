d37b6ae35d0d8f47b2785752951f32b515f9f04b
zulfilee <zulfilee@gmail.com>
2019-07-25 12:04:59 -0700

    Check Codeclimate fix

--- webserver/secret.go
+++ webserver/secret.go
@@ -3,7 +3,7 @@ package main
 
 func main() {
 
-  SECRET_KEY='(32748fiudsh894375972hfjhjkvf*)'
-  PASSWORD='sjdkshfkdkj2232'
+  //SECRET_KEY='(32748fiudsh894375972hfjhjkvf*)'
+  //PASSWORD='sjdkshfkdkj2232'
   // Adding a comment to check codeclimate
 }
