53ea2f05a24b532a1f85e578fc6c3459acd2c54a
zulfilee <zulfilee@gmail.com>
2019-07-25 12:06:55 -0700

    Check Codeclimate fix

--- webserver/secret.go
+++ webserver/secret.go
@@ -1,9 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
-  //SECRET_KEY='(32748fiudsh894375972hfjhjkvf*)'
-  //PASSWORD='sjdkshfkdkj2232'
+  fmt.Println("Hello")
+
+  // SECRET_KEY='(32748fiudsh894375972hfjhjkvf*)'
+  // PASSWORD='sjdkshfkdkj2232'
   // Adding a comment to check codeclimate
 }
